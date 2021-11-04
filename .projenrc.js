f7a1991412af13cb0af9d13a0fd5eca4b4e50751
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-11-04 11:58:51 +0530

    initial commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,9 +1,9 @@
 const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'Harsh Rohila',
-  authorAddress: 'harsh.rohila@mrisoftware.com',
+  authorAddress: 'rohilaharsh@gmail.com',
   cdkVersion: '1.95.2',
-  defaultReleaseBranch: 'main',
+  defaultReleaseBranch: 'master',
   name: 'cdk-secure-parameter-store',
   repositoryUrl: 'https://github.com/harsh.rohila/cdk-secure-parameter-store.git',
 
